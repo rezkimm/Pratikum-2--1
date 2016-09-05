ea535dd99fdeba87f9fb0ca6ff97852369b351b0
rezkimm <rizki.snutz@gmail.com>
2016-09-05 07:57:36 +0700

    Add files via upload

---
+++ modul2_arduino_4211501002.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, 1);
+  pinMode(12, 1);
+  pinMode(13, 1);
+}
+
+void loop() {
+  digitalWrite(8, 1);
+  digitalWrite(9, 1);
+  digitalWrite(10, 1);
+  digitalWrite(11, 0);
+  digitalWrite(12, 0);
+  digitalWrite(13, 0);
+}
